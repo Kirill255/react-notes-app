e8122974d8e3b3a47ea22446d41d91f3e0ebf6b3
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-03-26 00:07:22 +0300

    add masonry layout

--- src/components/NotesGrid.js
+++ src/components/NotesGrid.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import Masonry from "masonry-layout";
 import Note from "./Note";
 
-const NotesGrid = ({ notes, onNoteDelete }) => {
-  return (
-    <div className="grid">
-      {notes.map((note) => (
-        <Note key={note.id} id={note.id} color={note.color} onDelete={onNoteDelete}>
-          {note.text}
-        </Note>
-      ))}
-    </div>
-  );
-};
+class NotesGrid extends Component {
+  componentDidMount() {
+    this.masonry = new Masonry(this.grid, {
+      columnWidth: 240,
+      gutter: 10,
+      fitWidth: true // isFitWidth: true
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.notes !== this.props.notes) {
+      this.masonry.reloadItems();
+      this.masonry.layout();
+    }
+  }
+
+  componentWillUnmount() {
+    this.masonry.destroy();
+  }
+
+  setNotesGridRef = (ref) => {
+    this.grid = ref;
+  };
+
+  render() {
+    const { notes, onNoteDelete } = this.props;
+    return (
+      <div className="grid" ref={this.setNotesGridRef}>
+        {notes.map((note) => (
+          <Note key={note.id} id={note.id} color={note.color} onDelete={onNoteDelete}>
+            {note.text}
+          </Note>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default NotesGrid;
